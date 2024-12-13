2ac66f2eb678bda61af78a17324a4b0cd6171fbe
MahaGuru63 <mahalakshmi.g@responsive.io>
2024-12-13 10:47:59 +0530

    Fonts Correction added

--- app/ui/dashboard/sidenav.tsx
+++ app/ui/dashboard/sidenav.tsx
@@ -1,10 +1,12 @@
 import Link from 'next/link';
 import NavLinks from '@/app/ui/dashboard/nav-links';
 import { PowerIcon } from '@heroicons/react/24/outline';
+import AcmeLogo from '@/app/ui/acme-logo';
 
 export default function SideNav() {
   return (
     <div className="flex h-full flex-col px-3 py-4 md:px-2">
+      <AcmeLogo />
       <Link
         className="mb-2 flex h-20 items-end justify-start rounded-md bg-zinc-800 p-4 md:h-40"
         href="/"
